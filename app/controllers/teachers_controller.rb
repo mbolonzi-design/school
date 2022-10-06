07f7615a292469d75d60eb892fbb14f47465db7c
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-06 20:10:57 +0300

    feat: find specific teacher + create teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,4 +4,24 @@ class TeachersController < ApplicationController
         teachers = Teacher.all 
         render json: teachers, status: :ok
     end
+
+    def show
+        teacher = find_teacher
+        render json: teacher, status: :ok
+    end
+
+    def create
+        teacher = Teacher.create(teacher_params)
+        render json: teacher, status: :created
+    end
+
+    private 
+
+    def find_teacher
+        teachers = Teacher.find_by(id: params[:id])
+    end
+
+    def teacher_params
+        params.permit(:name, :email, :phone, :department)
+    end
 end

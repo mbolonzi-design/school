e7f32c6712c879ffc5537679e1d78d2ac2b3b72c
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-06 19:41:33 +0300

    feat: seed sample data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students only: [:index, :show, :create, :update, :destroy]
-  resources :courses only: [:index, :show, :create, :update, :destroy]
-  resources :teachers only: [:index, :show, :create, :update, :destroy]
+  resources :students, only: [:index, :show, :create, :update, :destroy]
+  resources :courses, only: [:index, :show, :create, :update, :destroy]
+  resources :teachers, only: [:index, :show, :create, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Listing students..."
+
+Student.create(name: "John Kiarie", email: "kiarie2016@yahoo.com", reg_no: "GSS/113410/CS/2016", course: "Full-Stack Software Development")
+Student.create(name: "Elachi Beatrice", email: "betty.elachi@gmail.com", reg_no: "GSS/123440/DS/2017", course: "Data Science")
+Student.create(name: "Alice Kamande", email: "alice016@gmail.com", reg_no: "GSS/233410/DEV/2018", course: "DevOps")
+
+puts "Listing teachers..."
+
+Teacher.create(name: "Jacob Mwangi", email: "jmwani@gmail.com", phone: "0712345678", department: "Computer Science")
+Teacher.create(name: "Mercy Namusonge", email: "namusonge@gmail.com", phone: "0712345678", department: "Political Science")
+Teacher.create(name: "Marcus Mane", email: "mane@gmail.com", phone: "0712345678", department: "Mathematics")
+
+puts "Listing courses..."
+
+Course.create(
+    course_name: "Full-Stack Software Development",
+    student_id: 1,
+    teacher_id: 1
+)

eb39d98d76580386aa823238c65760f9c7240378
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-06 19:55:28 +0300

    feat:add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
     has_many :courses
     has_many :teachers, through: :courses
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :reg_no, uniqueness: true
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,8 @@
 class Teacher < ApplicationRecord
     has_many :courses
     has_many :students, through: :courses
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :department, presence: true
 end

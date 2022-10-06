b1b5259de006fbd998a2af4cab0b59ff8d3a5e24
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-06 19:17:30 +0300

    feat:add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :students only: [:index, :show, :create, :update, :destroy]
+  resources :courses only: [:index, :show, :create, :update, :destroy]
+  resources :teachers only: [:index, :show, :create, :update, :destroy]
 end

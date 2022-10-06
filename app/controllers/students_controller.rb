e0bd0a54af51fa01ed3255d15e58875cd2558c74
mbolonzi-design <mbolonzi.design@gmail.com>
2022-10-06 20:06:31 +0300

    feat: find student by id + create new student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,24 @@ class StudentsController < ApplicationController
         students = Student.all 
         render json: students, status: :ok
     end
+
+    def show
+        student = find_student
+        render json: student, status: :ok
+    end
+
+    def create
+        student = Student.create(student_params)
+        render json: student, status: :created
+    end
+
+    private 
+
+    def find_student
+        students = Student.find_by(id: params[:id])
+    end
+
+    def student_params
+        params.permit(:name, :email, :reg_no)
+    end
 end
